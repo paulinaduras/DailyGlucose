cf737442279548b8eccfa13764376c08fa644af9
Ananalina <paulina.duras@onet.pl>
2021-05-25 22:03:21 +0200

    Poprawiony Ekran Startu + Logowania

--- app/src/main/java/com/example/dailyglucose/EkranLogowania.kt
+++ app/src/main/java/com/example/dailyglucose/EkranLogowania.kt
@@ -1,7 +1,9 @@
 package com.example.dailyglucose
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 
@@ -9,8 +11,8 @@ class EkranLogowania : AppCompatActivity() {
 
     private var logowanieNazwaUzytkownika: EditText? = null
     private var logowanieHaslo: EditText? = null
-    private var btnLogowanie1: Button? = null
-    private var btnLogowanie2: Button? = null
+    private var btnLogowania: Button? = null
+    private var btnPowrotu: Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,8 +20,19 @@ class EkranLogowania : AppCompatActivity() {
 
         logowanieNazwaUzytkownika = findViewById(R.id.textLogowanie3)
         logowanieHaslo = findViewById(R.id.textLogowanie5)
-        btnLogowanie1 = findViewById(R.id.btnLogowanie1)
-        btnLogowanie2 = findViewById(R.id.btnLogowanie2)
+        btnLogowania = findViewById(R.id.btnLogowania)
+        btnPowrotu = findViewById(R.id.btnPowrotu)
 
+        btnPowrotu?.setOnClickListener(object : View.OnClickListener{
+            override fun onClick(v: View?) {
+                openMainActivity()
+            }
+        })
+    }
+
+    // ta metoda pozwala na przechodzenie z obecnego widoku na widok MainActivity
+    private fun openMainActivity(){
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
     }
 }
