511f51dc1107e935ccb9f95b05c12091904e931f
Ananalina <paulina.duras@onet.pl>
2021-05-26 10:58:42 +0200

    Start + Logowanie + Rejestracja + Główny + NowyGlukoza + NowyInsulina + Obie Historie

--- app/src/main/java/com/example/dailyglucose/EkranHistoriaGlukozy.kt
+++ app/src/main/java/com/example/dailyglucose/EkranHistoriaGlukozy.kt
@@ -1,11 +1,46 @@
 package com.example.dailyglucose
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class EkranHistoriaGlukozy : AppCompatActivity() {
+
+    private var btnNowyGLukoza: Button? = null
+    private var btnPowrot: Button? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ekran_historia_glukozy)
+
+        btnNowyGLukoza = findViewById(R.id.btnHistoriaGlukozy1)
+        btnPowrot = findViewById(R.id.btnHistoriaGlukoza2)
+
+
+        btnNowyGLukoza?.setOnClickListener(object : View.OnClickListener{
+            override fun onClick(v: View?) {
+                oknoDodawaniaGlukozy()
+            }
+        })
+
+        btnPowrot?.setOnClickListener(object : View.OnClickListener{
+            override fun onClick(v: View?) {
+                openEkranGlowny()
+            }
+        })
+
+
+    }
+
+    private fun oknoDodawaniaGlukozy(){
+        val intent = Intent(this, EkranNowyGlukoza::class.java)
+        startActivity(intent)
+    }
+
+    private  fun openEkranGlowny(){
+        val intent = Intent(this, EkranGlowny::class.java)
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/example/dailyglucose/EkranHistoriaInsuliny.kt
+++ app/src/main/java/com/example/dailyglucose/EkranHistoriaInsuliny.kt
@@ -1,11 +1,45 @@
 package com.example.dailyglucose
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class EkranHistoriaInsuliny : AppCompatActivity() {
+
+    private var btnNowyInsulina: Button? = null
+    private var btnPowrot: Button? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ekran_historia_insuliny)
+
+        btnNowyInsulina = findViewById(R.id.btnHistoriaInsuliny1)
+        btnPowrot = findViewById(R.id.btnHistoriaInsuliny2)
+
+
+        btnNowyInsulina?.setOnClickListener(object : View.OnClickListener{
+            override fun onClick(v: View?) {
+                openOknoDodawaniaInsuliny()
+            }
+        })
+
+        btnPowrot?.setOnClickListener(object : View.OnClickListener{
+            override fun onClick(v: View?) {
+                openEkranGlowny()
+            }
+        })
+
+    }
+
+    private fun openOknoDodawaniaInsuliny(){
+        val intent = Intent(this, EkranNowyInsulina::class.java)
+        startActivity(intent)
+    }
+
+    private  fun openEkranGlowny(){
+        val intent = Intent(this, EkranGlowny::class.java)
+        startActivity(intent)
     }
 }
