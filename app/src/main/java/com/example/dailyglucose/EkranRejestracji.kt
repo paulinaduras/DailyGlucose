b29900d8b2647bec36d06d13b106232b26964cea
Ananalina <paulina.duras@onet.pl>
2021-06-08 19:13:44 +0200

    Rejestracja

--- app/src/main/java/com/example/dailyglucose/EkranRejestracji.kt
+++ app/src/main/java/com/example/dailyglucose/EkranRejestracji.kt
@@ -1,13 +1,18 @@
 package com.example.dailyglucose
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
-class EkranRejestracji : AppCompatActivity() {
+class EkranRejestracji : BaseActivity() {
 
     private var rejestracjaNazwaUzytkownika: EditText? = null
     private var rejestracjaHaslo: EditText? = null
@@ -29,7 +34,8 @@ class EkranRejestracji : AppCompatActivity() {
 
         btnRejestracji?.setOnClickListener(object : View.OnClickListener{
             override fun onClick(v: View?) {
-                logowanie()
+                registerUser()
+//                openEkranLogowania()
             }
         })
 
@@ -40,13 +46,86 @@ class EkranRejestracji : AppCompatActivity() {
         })
     }
 
-    private fun logowanie(){
-        val intent = Intent(this, EkranGlowny::class.java)
-        startActivity(intent)
+    private fun validateRegisterDetails(): Boolean {
+
+        return when{
+            TextUtils.isEmpty(rejestracjaEmail?.text.toString().trim{ it <= ' '}) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_email),true)
+                false
+            }
+            TextUtils.isEmpty(rejestracjaNazwaUzytkownika?.text.toString().trim{ it <= ' '}) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_name),true)
+                false
+            }
+
+            TextUtils.isEmpty(rejestracjaHaslo?.text.toString().trim{ it <= ' '}) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_password),true)
+                false
+            }
+
+            TextUtils.isEmpty(rejestracjaPowtorzHaslo?.text.toString().trim{ it <= ' '}) -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_reppassword),true)
+                false
+            }
+
+            rejestracjaHaslo?.text.toString().trim {it <= ' '} != rejestracjaPowtorzHaslo?.text.toString().trim{it <= ' '} -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_password_mismatch),true)
+                false
+            }
+
+
+            else -> {
+                //showErrorSnackBar("Your details are valid",false)
+                true
+            }
+        }
+    }
+
+
+    private fun registerUser(){
+        if (validateRegisterDetails()){
+            val login: String = rejestracjaEmail?.text.toString().trim() {it <= ' '}
+            val password: String = rejestracjaHaslo?.text.toString().trim() {it <= ' '}
+
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(login,password).addOnCompleteListener(
+                OnCompleteListener <AuthResult>{ task ->
+                    if(task.isSuccessful){
+                        val firebaseUser: FirebaseUser = task.result!!.user!!
+
+//                        val user = User(
+//                            firebaseUser.uid,
+//                            rejestracjaNazwaUzytkownika?.text.toString().trim() {it <= ' '},
+//                            rejestracjaEmail?.text.toString().trim() {it <= ' '},
+//                        )
+
+
+//                        FireStoreClass().registerUser(this,user)
+
+                        showErrorSnackBar("Twoja rejestracja przebiegła pomyślnie.",false)
+                        //FirebaseAuth.getInstance().signOut()
+//                        finish()
+
+
+                    } else{
+                        showErrorSnackBar(task.exception!!.message.toString(),true)
+                    }
+                }
+            )
+        }
     }
 
     private fun openEkranLogowania(){
         val intent = Intent(this, EkranLogowania::class.java)
         startActivity(intent)
+    }
+
+    fun userRegistrationSuccess(){
+
+        Toast.makeText(
+            this@EkranRejestracji,
+            resources.getString(R.string.register_success),
+            Toast.LENGTH_SHORT
+        ).show()
+
     }
 }
